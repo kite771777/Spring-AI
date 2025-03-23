3cb6c901d32377415b96018ea542ed4ee54c73b1
Ritik Raj <ritikraj@Ritiks-MacBook-Air.local>
2025-03-23 23:54:31 +0530

    Initial commit for spring ai project

---
+++ src/main/java/com/ai/SpringAi/Controller/Health.java
@@ -0,0 +1,14 @@
+package com.ai.SpringAi.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Health {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Spring AI Application is running";
+    }
+}
---
+++ src/main/java/com/ai/SpringAi/SpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.ai.SpringAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiApplication.class, args);
+	}
+}
